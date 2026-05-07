f28c95c6067c1e2cc85ce218e7c8708364a241a4
albapaulina150-crypto <albapaulina150@gmail.com>
2026-05-07 15:12:10 -0600

    Metodo grad

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 def metodo_gradiente(n: int, x0: list, a: float, f: callable) -> list:
     
     x = np.array(x0, dtype=float)
-    trayectoria = [x.copy()]
+    lista = [x.copy()]
 
     for _ in range(n):
         gradiente = np.array(gradf(x.tolist(), f))
         x = x - alpha * gradiente
-        trayectoria.append(x.copy())
-
-    return trayectoria
+        lista.append(x.copy())
