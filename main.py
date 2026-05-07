232ebbc8384f4ef9a1ad08e8a70d5bd8fed935b2
albapaulina150-crypto <albapaulina150@gmail.com>
2026-05-07 09:54:30 -0600

    Implemnetacion funciones

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+def metodo_gradiente(n: int, x0: list, a: float, f: callable) -> list:
+    
+    x = np.array(x0, dtype=float)
+    trayectoria = [x.copy()]
 
+    for _ in range(n):
+        gradiente = np.array(gradf(x.tolist(), f))
+        x = x - alpha * gradiente
+        trayectoria.append(x.copy())
+
+    return trayectoria
